14b7292bef6f6b420437e1c04a9ead780d3f6d5c
Sudheer Chittuluri <Sudheer.Chittuluri@hhsc.state.tx.us>
2020-04-27 11:46:01 -0500

    Add project files.

---
+++ EventandDelegateExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EventandDelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var video=new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder();
+            
+
+            MessageService ms = new MessageService();
+            videoEncoder.VideoEncoded += ms.onVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+
+    public class MessageService
+    {
+        public void onVideoEncoded(object sender,EventArgs e)
+        {
+            Console.WriteLine("Message sending....");
+        }
+    }
+}
